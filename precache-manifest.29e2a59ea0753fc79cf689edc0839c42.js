4cb934e0a68433b67696849f154299a6a3dbc4d4
Bobrek <36887372+Bobrek@users.noreply.github.com>
2019-04-10 22:30:21 +0100

    Updates

---
+++ precache-manifest.29e2a59ea0753fc79cf689edc0839c42.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa42551b76a87b7e306fd33b97d4204a",
+    "url": "/resume/static/media/java.fa42551b.jpg"
+  },
+  {
+    "revision": "f2854ccd35184326d498",
+    "url": "/resume/static/css/main.9343ffac.chunk.css"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "90ca90ad0765bf5b351e3b4542a92849",
+    "url": "/resume/static/media/PHP.90ca90ad.png"
+  },
+  {
+    "revision": "4eeb03c14c7ef3a39c29",
+    "url": "/resume/static/js/2.c76f0542.chunk.js"
+  },
+  {
+    "revision": "8c1b868dfa0ce11c8dcbc727be2ea6f5",
+    "url": "/resume/static/media/coast.8c1b868d.jpg"
+  },
+  {
+    "revision": "f2854ccd35184326d498",
+    "url": "/resume/static/js/main.e8a7301e.chunk.js"
+  },
+  {
+    "revision": "87d5873e077d27e23f4e1621a0010341",
+    "url": "/resume/static/media/css.87d5873e.png"
+  },
+  {
+    "revision": "d14e4fd3c8b856f05612643d6773e497",
+    "url": "/resume/static/media/react.d14e4fd3.png"
+  },
+  {
+    "revision": "95cb9f7595610a69a23082974cf4fbd6",
+    "url": "/resume/static/media/js.95cb9f75.png"
+  },
+  {
+    "revision": "4eeb03c14c7ef3a39c29",
+    "url": "/resume/static/css/2.f4e0f636.chunk.css"
+  },
+  {
+    "revision": "4f76e22e4fd5f60fffb88fced372a1d2",
+    "url": "/resume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b078c8900b771b100cc20035cbb9344.js"
+  "/resume/precache-manifest.29e2a59ea0753fc79cf689edc0839c42.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/resume/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
